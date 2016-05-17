e31586d8e7822ab945d9bef52df6693ac65d849f
RaspiKidd <kerry@raspikidd.com>
2016-05-17 22:44:45 +0000

    LedBlink script updated with all 5 LED's

--- SoundInstall/LedBlink.py
+++ SoundInstall/LedBlink.py
@@ -1,10 +1,36 @@
 from gpiozero import LED
 from time import sleep
 
-led = LED(17)
+led1 = LED( 18)
+led2 = LED (25)
+led3 = LED (12)
+led4 = LED (17)
+led5 = LED (21)
 
 while True:
-	led.on ()
-	sleep (0.25)
-	led.off ()
-	sleep (1)
+	led1.on ()
+	sleep (0.1)
+	led1.off ()
+	sleep (0.5)
+	
+	led2.on ()
+	sleep (0.1)
+	led2.off ()
+	sleep (0.5)
+	
+	led3.on ()
+	sleep (0.1)
+	led3.off ()
+	sleep (0.5)
+
+	led4.on ()
+	sleep (0.1)
+	led4.off ()
+	sleep (0.5)
+
+	led5.on ()
+	sleep (0.1)
+	led5.off ()
+	sleep (0.5)
+
+	
